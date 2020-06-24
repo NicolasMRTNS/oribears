bde6a56db958e91f850e07c3f0410064b472ed1c
Nicolas Martins <nicolas.martins@hotmail.fr>
2020-06-24 10:18:27 +0200

    products page ok with while loop

--- scripts/products.js
+++ scripts/products.js
@@ -1,30 +1,46 @@
+// Encrage du HTML
 let products = document.getElementById("products");
-let url = "http://localhost:3000/api/teddies";
+// Class Bootstrap des éléments
 const imgClass = ["img-fluid", "mx-auto"];
 const btnClass = ["btn", "btn-secondary"];
+const articleClass = ["mb-5"];
 
 // Fetch à l'API
-fetch(url)
+fetch("http://localhost:3000/api/teddies")
   .then((response) => response.json())
   .then((response) => {
-    let productPicture = document.createElement("img");
-    productPicture.src = response[0].imageUrl;
-    productPicture.classList.add(...imgClass);
-    let productName = document.createElement("h3");
-    productName.innerHTML = response[0].name;
-    let productPrice = document.createElement("p");
-    productPrice.innerHTML = `Prix : ${response[0].price / 100}€`;
-    let productDescription = document.createElement("p");
-    productDescription.innerHTML = response[0].description;
-    let productBtn = document.createElement("button");
-    productBtn.innerHTML = "Voir la page produit";
-    productBtn.classList.add(...btnClass);
-    let newArticle = document.createElement("article");
-    newArticle.innerHTML =
-      productPicture.outerHTML +
-      productName.outerHTML +
-      productPrice.outerHTML +
-      productDescription.outerHTML +
-      productBtn.outerHTML;
-    products.appendChild(newArticle);
+    // Boucle while pour afficher tous les produits
+    let i = 0;
+    while (i < response.length) {
+      // Création d'une image et affichage de celle-ci
+      let productPicture = document.createElement("img");
+      productPicture.src = response[i].imageUrl;
+      productPicture.classList.add(...imgClass);
+      // Création d'un H3 pour le nom du produit
+      let productName = document.createElement("h3");
+      productName.innerHTML = response[i].name;
+      // Création d'un paragraphe pour y afficher le prix
+      let productPrice = document.createElement("p");
+      productPrice.innerHTML = `Prix : ${response[i].price / 100}€`;
+      // Création d'un deuxième paragraphe pour y afficher la description
+      let productDescription = document.createElement("p");
+      productDescription.innerHTML = response[i].description;
+      // Création du bouton pour la page produit
+      let productBtn = document.createElement("button");
+      productBtn.innerHTML = "Voir la page produit";
+      productBtn.classList.add(...btnClass);
+      // Tout cela va dans un article
+      let newArticle = document.createElement("article");
+      newArticle.innerHTML =
+        productPicture.outerHTML +
+        productName.outerHTML +
+        productPrice.outerHTML +
+        productDescription.outerHTML +
+        productBtn.outerHTML;
+      newArticle.classList.add(articleClass);
+      // On ajoute cette article à notre encrage du HTML
+      products.appendChild(newArticle);
+      // On incrémente i pour éviter une boucle infinie
+      i++;
+    }
   });
